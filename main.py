c29e2d60863604b03eb8c9f318ccb6321266f567
Venobet Rhosta <157577285+vnobets7@users.noreply.github.com>
2024-08-08 21:31:32 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import connection
+import sqlparse
+import pandas as pd
+
+if __name__ == '__main__':
+    # connection data source
+    conf = connection.config('marketplace_prod')
+    conn, engine = connection.get_conn(conf, 'DataSource')
+    cursor = conn.cursor()
+    
+    # connection dwh
+    conf_dwh = connection.config('dwh')
+    conn_dwh, engine_dwh = connection.get_conn(conf_dwh, 'DWH')
+    cursor_dwh = conn.cursor()
+    
+    # get query string
+    path_query = os.getcwd() + '/query/'
+    query = sqlparse.format(
+        open(path_query + 'query.sql', 'r').read(), strip_comments=True
+    ).strip()
+    dwh_design = sqlparse.format(
+        open(path_query + 'dwh_design.sql', 'r').read(), strip_comments=True
+    ).strip()
+    
+    try:
+        #get data
+        print('[INFO] service etl is running...')
+        df = pd.read_sql(query, engine)
+        print(df)
+    #     # create schema dwh
+    #     cursor_dwh.execute(dwh_design)
+    #     conn_dwh.commit()
+        
+    #     # ingest data to dwh
+    #     df.to_sql(
+    #         'dim_orders_ricky',
+    #         engine_dwh,
+    #         schema='public',
+    #         if_exists='append', # replace or append
+    #         index=False
+    #     )
+        
+    #     print('[INFO] service etl is success...')
+        
+    except Exception as e:
+        print("[INFO] service etl is failed")
+        print(str(e))
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Deskripsi Mini-Project1
+Bagian dari tugas Fast-Track, dibuat oleh tim Insight Architect.
+
+## Insight Architect - Anggota
+|  Nama | Kelompok 
+| ------------ | ------------ |
+| Ricky Suhanry | 2 |
+| xyz | 2 |
+| abc | 2 |
+| ued | 2 |
+| xor | 2 |
